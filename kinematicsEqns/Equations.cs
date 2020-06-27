4c1182244db2afda225ca28864b345de842a59cc
Lakshan Fernando <lakshanf@microsoft.com>
2020-06-27 15:06:23 -0700

    Add project files.

---
+++ kinematicsEqns/Equations.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace kinematicsEqns
+{
+    public class Equations
+    {
+        const double g = -9.8;
+
+        double? a;
+        double? v_0;
+        double? v_f;
+        double? s;
+        double? time;
+
+        bool _vertical;
+
+        MetricType _unassigned = MetricType.None;
+
+        public double? Result { get; internal set; }
+
+        public Equations(bool vertical)
+        {
+            _vertical = vertical;
+            if (_vertical)
+            {
+                a = g;
+            }
+            else
+            {
+                a = 0;
+            }
+        }
+
+        internal void GetTime()
+        {
+            GetParms(MetricType.Time);
+            switch (_unassigned) 
+            {
+                case MetricType.Distance:
+                    // v_f = v_0 + a*t
+                    time = (v_f - v_0) / a;
+                    break;
+                case MetricType.FinalVelocity:
+                    // x = v_0*t + 1/2(a*t^2)
+                    // A*x^2 + B*x + C=0
+                    double A = a.Value / 2;
+                    double B = v_0.Value;
+                    double C = -s.Value;
+
+                    double t1 = Math.Abs(Math.Sqrt(Math.Pow(B, 2.0) - 4 * A * C));
+                    double t2 = (-B + t1)/(2*A);
+                    if (t2 > 0)
+                    {
+                        time = t2;
+                    }
+                    else
+                    {
+                        t2 = (-B - t1) / (2 * A);
+                        Debug.Assert(t2 >= 0, "Hmmmm... time problems");
+                        time = t2;
+                    }
+                    break;
+            }
+
+            Result = time;
+        }
+
+
+        internal void GetDistance()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void GetInitialVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void GetFinalVelocity()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void GetParms(MetricType metric)
+        {
+            int skippedValues = 0;
+            if (metric != MetricType.Time)
+            {
+                Console.WriteLine("Please enter Time: ");
+                time = ReadUserInput();
+                if (!time.HasValue)
+                {
+                    skippedValues++;
+                    _unassigned = MetricType.Time;
+                }
+            }
+            if (metric != MetricType.Distance)
+            {
+                Console.WriteLine("Please enter distance: ");
+                s  = ReadUserInput();
+                if (!s.HasValue)
+                {
+                    skippedValues++;
+                    _unassigned = MetricType.Distance;
+                }
+            }
+            if (metric != MetricType.InitialVelocity)
+            {
+                Console.WriteLine("Please enter Initial Velocity: ");
+                v_0 = ReadUserInput();
+                if (!v_0.HasValue)
+                {
+                    skippedValues++;
+                    _unassigned = MetricType.InitialVelocity;
+                }
+            }
+            if (metric != MetricType.FinalVelocity)
+            {
+                Console.WriteLine("Please enter Final Velocity: ");
+                v_f = ReadUserInput();
+                if (!v_f.HasValue)
+                {
+                    skippedValues++;
+                    _unassigned = MetricType.FinalVelocity;
+                }
+            }
+            if (skippedValues > 1)
+            {
+                throw new ArgumentException("Not sufficient Information to proceed");
+            }
+        }
+
+        /// <summary>
+        /// Reads the user input
+        /// </summary>
+        /// <param name="skippedValues"></param>
+        /// <returns></returns>
+        private double? ReadUserInput()
+        {
+            double? ret = null;
+            Console.WriteLine("Enter empty string if needed");
+            string value = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(value))
+            {
+                ret = Double.Parse(value);
+            }
+            return ret;
+        }
+
+        enum MetricType
+        {
+            None,
+            Time,
+            Distance,
+            InitialVelocity,
+            FinalVelocity,
+            Acceleration
+        }
+
+    }
+}
---
+++ kinematicsEqns/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace kinematicsEqns
+{
+    /// <summary>
+    /// Solving the kinematic eqns programmatically
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string option;
+            bool vertical = true;
+            
+            Console.WriteLine("0-time, 1-distance, 2-v_0, 3-v_f");
+            option = Console.ReadLine();
+
+            Console.WriteLine("Is the movement horizontal? 1 - yes");
+            var s = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(s) && int.Parse(s)==1)
+            {
+                vertical = false;
+            }
+
+            Equations eqn = new Equations(vertical);
+            switch (option)
+            {
+                case "0":
+                    eqn.GetTime();
+                    break;
+                case "1":
+                    eqn.GetDistance();
+                    break;
+                case "2":
+                    eqn.GetInitialVelocity();
+                    break;
+                case "3":
+                    eqn.GetFinalVelocity();
+                    break;
+            }
+            Console.WriteLine($"Result: <{eqn.Result}>");
+        }
+    }
+}
